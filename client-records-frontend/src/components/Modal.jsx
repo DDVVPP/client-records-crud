929d519b0c5b2fe2aedee0157c27ebe06c6ad635
DDVVPP <darshinvp@gmail.com>
2024-12-08 12:49:04 -0800

    feat: added inputs to modal

--- client-records-frontend/src/components/Modal.jsx
+++ client-records-frontend/src/components/Modal.jsx
@@ -1,12 +1,82 @@
-const Modal = ({ isOpen, onClose, mode, onSubmit }) => {
+import { useState } from "react";
+
+const Modal = ({ isOpen, onClose, mode }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [job, setJob] = useState("");
+  const [rate, setRate] = useState(0);
+  const [status, setStatus] = useState(true);
+
+  const handleStatusChange = (e) => {
+    setStatus(e.target.value === "Active");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onClose();
+  };
+
   return (
     <dialog id="my_modal_3" className="modal" open={isOpen}>
       <div className="modal-box">
-        <h3 className="font-bold text-lg py-4">
+        <h3 className="font-bold text-lg mb-4">
           {mode === "edit" ? "Edit Client" : "Client Details"}
         </h3>
 
-        <form method="dialog">
+        <form
+          method="dialog"
+          className="flex flex-col gap-y-4"
+          onSubmit={handleSubmit}
+        >
+          <label className="input input-bordered flex items-center gap-2">
+            Name
+            <input
+              type="text"
+              className="grow"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            Email
+            <input
+              type="text"
+              className="grow"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            Job
+            <input
+              type="text"
+              className="grow"
+              value={job}
+              onChange={(e) => setJob(e.target.value)}
+            />
+          </label>
+
+          <div className="flex justify-between gap-4">
+            {" "}
+            <label className="input input-bordered flex items-center gap-2">
+              Rate
+              <input
+                type="number"
+                className="grow"
+                value={rate}
+                onChange={(e) => setRate(e.target.value)}
+              />
+            </label>
+            <select
+              className="select select-bordered w-full max-w-xs"
+              value={status ? "Active" : "Inactive"}
+              onChange={handleStatusChange}
+            >
+              <option>Inactive</option>
+              <option>Active</option>
+            </select>
+          </div>
+
           <button
             className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
             onClick={onClose}
