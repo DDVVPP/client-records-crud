76676935717225618ad974115f3cbf58791d1c17
DDVVPP <darshinvp@gmail.com>
2024-12-08 16:43:10 -0800

    feat: added create and update functions and routes

--- client-records-backend/src/controllers/clientController.js
+++ client-records-backend/src/controllers/clientController.js
@@ -9,3 +9,34 @@ export const getClients = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const createClient = async (req, res) => {
+  try {
+    const clientData = req.body;
+    const newClient = await clientService.createClient(clientData);
+    res.status(200).json(newClient);
+  } catch (error) {
+    console.error("Error creating clients:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const updateClient = async (req, res) => {
+  try {
+    const clientId = req.params.id;
+    const clientData = req.body;
+
+    const updatedClient = await clientService.updateClient(
+      clientId,
+      clientData
+    );
+
+    if (!updatedClient) {
+      return res.status(404).json({ message: "Client not found" });
+    }
+    res.status(200).json(updatedClient);
+  } catch (error) {
+    console.error("Error updating clients:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- client-records-backend/src/services/clientServices.js
+++ client-records-backend/src/services/clientServices.js
@@ -4,3 +4,23 @@ export const getClients = async () => {
   const { rows } = await query("SELECT * FROM clients_tb");
   return rows;
 };
+
+export const createClient = async (clientData) => {
+  const { name, email, job, rate, isactive } = clientData;
+  const { rows } = await query(
+    "INSERT INTO clients_tb (name, email, job, rate, isactive) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+    [name, email, job, rate, isactive]
+  );
+
+  return rows[0];
+};
+
+export const updateClient = async (clientId, clientData) => {
+  const { name, email, job, rate, isactive } = clientData;
+  const { rows } = await query(
+    "UPDATE clients_tb SET name = $1, email = $2, job = $3, rate = $4, isactive = $5 WHERE id = $6 RETURNING *",
+    [name, email, job, rate, isactive, clientId]
+  );
+  console.log("rows", rows[0]);
+  return rows[0];
+};
